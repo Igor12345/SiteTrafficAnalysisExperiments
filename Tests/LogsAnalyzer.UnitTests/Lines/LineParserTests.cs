2e0a8d9cc7338a3e697b37710367cffccd7bc7f1
Igor Solovev <igorth@gmail.com>
2024-10-20 00:22:05 +0200

    Updated unit tests for LineParser

--- Tests/LogsAnalyzer.UnitTests/Lines/LineParserTests.cs
+++ Tests/LogsAnalyzer.UnitTests/Lines/LineParserTests.cs
@@ -5,7 +5,7 @@ namespace LogsAnalyzer.UnitTests.Lines
     public class LineParserTests
     {
         [Test]
-        public void ShouldParseCorrectLine()
+        public void ShouldParseCorrectLineShortVersion()
         {
             LineParser parser = new LineParser(";");
             var customerId = 123456;
@@ -22,11 +22,41 @@ namespace LogsAnalyzer.UnitTests.Lines
         [TestCase("some time;123", "Invalid line")]
         [TestCase("some time;abc123;456", "Invalid customer id:")]
         [TestCase("some time;123;-456", "Invalid page id:")]
-        public void ShouldNotParseInCorrectLine(string line, string partOfErrorMessage)
+        public void ShouldNotParseInCorrectLineShortVersion(string line, string partOfErrorMessage)
         {
             LineParser parser = new LineParser(";");
 
             var parsingResult = parser.ParseShort(line);
+
+            Assert.That(parsingResult.Success, Is.False);
+            Assert.That(parsingResult.ErrorMessage, Is.Not.Null);
+            Assert.That(parsingResult.ErrorMessage.Contains(partOfErrorMessage), Is.True);
+        }
+
+        [Test]
+        public void ShouldParseCorrectLineFullVersion()
+        {
+            LineParser parser = new LineParser(";");
+            var customerId = 123456;
+            var pageId = 98765;
+
+            var parsingResult = parser.ParseShort($"2024-10-19T22:19:31;{customerId};{pageId}");
+
+            Assert.That(parsingResult.Success, Is.True);
+            Assert.That(parsingResult.Value.customerId, Is.EqualTo(customerId));
+            Assert.That(parsingResult.Value.pageId, Is.EqualTo(pageId));
+        }
+
+        [Test]
+        [TestCase("2024-10-19T22:19:31;123", "Invalid line")]
+        [TestCase("some time;abc123;456", "Invalid time mark:")]
+        [TestCase("2024-10-19T22:19:31;abc123;456", "Invalid customer id:")]
+        [TestCase("2024-10-19T22:19:31;123;-456", "Invalid page id:")]
+        public void ShouldNotParseInCorrectLineFullVersion(string line, string partOfErrorMessage)
+        {
+            LineParser parser = new LineParser(";");
+
+            var parsingResult = parser.Parse(line);
 
             Assert.That(parsingResult.Success, Is.False);
             Assert.That(parsingResult.ErrorMessage, Is.Not.Null);
